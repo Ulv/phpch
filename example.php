742b248700a42d19a2aeee419db0f0c49e5f72c7
Volodymyr Chmil <chmil@smartproject.ua>
2022-12-28 14:59:56 +0200

    Example update

--- example.php
+++ example.php
@@ -1,17 +1,28 @@
 <?php
 // usage example
 
-include __DIR__ . '/vendor/autoload.php';
+ini_set('display_errors', E_ALL);
 
-$ch = new \Ulv\Phpch\Client();
-$ch->query('select * from currency_exchange_rates ');// ->fetchAll();
+include_once __DIR__ . '/vendor/autoload.php';
 
-foreach ($ch->cursor() as $one) {
-    var_dump(convert(memory_get_usage(true)), $one);
+$connection = new \Ulv\Phpch\Configuration([
+    'host'            => '172.16.238.2',
+    'server.database' => 'devel',
+]);
+
+$ch = new \Ulv\Phpch\Client($connection);
+
+$ch->query('select date, user_id, device_id, name from tracker where date between \'2022-04-01\' and \'2022-04-10\' ');
+
+$i = 0;
+foreach ($ch->stream() as $one) {
+    var_dump($one);
+    var_dump($i . ' ' . convert(memory_get_usage(true)));
+    $i++;
 }
 
 function convert($size)
 {
-    $unit=array('b','kb','mb','gb','tb','pb');
-    return @round($size/pow(1024,($i=floor(log($size,1024)))),2).' '.$unit[$i];
+    $unit = ['b', 'kb', 'mb', 'gb', 'tb', 'pb'];
+    return @round($size / pow(1024, ($i = floor(log($size, 1024)))), 2) . ' ' . $unit[$i];
 }
